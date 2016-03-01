c841640e75047eb89e464a0db056370b085b4e48
Dominik Deka <dominikdeka@gmail.com>
2016-03-01 21:47:03 +0100

    pi-blaster.js added

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,53 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+var piblaster = require('pi-blaster.js');
 
 router.post('/drive', function(req, res, next) {
   console.log('driving', req.body.direction);
   res.send('');
+  switch(req.body.direction){
+    case 'up':
+piblaster.setPwm(22, 0 );
+piblaster.setPwm(24, 0 );
+piblaster.setPwm(23, 0.5 );
+piblaster.setPwm(25, 0.5 );
+setTimeout(function(){
+        piblaster.setPwm(23, 0 );
+        piblaster.setPwm(25, 0 );
+},1000);
+      break;
+
+    case 'down':
+piblaster.setPwm(22, 1 );
+piblaster.setPwm(24, 1 );
+piblaster.setPwm(23, 0.5 );
+piblaster.setPwm(25, 0.5 );
+setTimeout(function(){
+        piblaster.setPwm(23, 0 );
+        piblaster.setPwm(25, 0 );
+},1000);
+      break;
+    case 'right':
+piblaster.setPwm(22, 1 );
+piblaster.setPwm(24, 0 );
+piblaster.setPwm(23, 0.5 );
+piblaster.setPwm(25, 0.5 );
+setTimeout(function(){
+        piblaster.setPwm(23, 0 );
+        piblaster.setPwm(25, 0 );
+},250);
+      break;
+    case 'left':
+piblaster.setPwm(22, 0 );
+piblaster.setPwm(24, 1 );
+piblaster.setPwm(23, 0.5 );
+piblaster.setPwm(25, 0.5 );
+setTimeout(function(){
+        piblaster.setPwm(23, 0 );
+        piblaster.setPwm(25, 0 );
+},250);
+      break;
+  }  
 });
 
 module.exports = router;
